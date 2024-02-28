78d3b9df23123fc9b5d948289151cb003471edf2
Joshua Sayen <sayenmarkjoshua@gmail.com>
2024-02-28 22:22:35 +0800

    commit 10:22

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //new
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.black87,
