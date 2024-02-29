21b5d0336325e7a76bc35e944d7fd6996a838f61
Joshua Sayen <sayenmarkjoshua@gmail.com>
2024-02-29 15:04:32 +0800

    New commit

--- lib/pages/description_page.dart
+++ lib/pages/description_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:flutter_application_1/class/item_class.dart';
 import 'package:flutter_application_1/core/constant.dart';
 
-class DescriptionPage extends StatelessWidget {
+class DescriptionPage extends StatefulWidget {
   const DescriptionPage({super.key, required this.box});
 
   final ItemClass box;
+
+  @override
+  State<DescriptionPage> createState() => _DescriptionPageState();
+}
+
+class _DescriptionPageState extends State<DescriptionPage> {
+  double fontSizeCustom = 25;
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +22,96 @@ class DescriptionPage extends StatelessWidget {
       appBar: AppBar(
         // use this if no return after clicking card -- automaticallyImplyLeading: false,
         // use this i want to center the title -- centerTitle: true,
-        title: Text(box.title),
+        title: Text(widget.box.title),
+        // for color -- backgroundColor: Colors.grey[400],
+        /*
+        //for custom IconButton customize
+        
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.dangerous),
+        ),
+        */
+        actions: [
+          IconButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Hello snackbar'),
+                    //duration: Duration(),
+                    behavior: SnackBarBehavior.floating,
+                  ),
+                );
+              },
+              icon: Icon(Icons.info)),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(kDouble10),
           child: Column(
             children: [
-              Image.asset(box.imagePath),
+              Image.asset(widget.box.imagePath),
+              Wrap(
+                spacing: kDouble10,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        fontSizeCustom = 25;
+                      });
+                    },
+                    child: const Text(
+                      'Small title',
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        fontSizeCustom = 35;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[100],
+                      foregroundColor: Colors.black,
+                    ),
+                    child: const Text(
+                      'Medium title',
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        fontSizeCustom = 100;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text(
+                      'Large title',
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      setState(() {
+                        fontSizeCustom = 200;
+                      });
+                    },
+                    child: Text(
+                      'Huge title',
+                    ),
+                  ),
+                ],
+              ),
               FittedBox(
                 child: Text(
-                  box.title,
-                  style: const TextStyle(
-                    fontSize: 200,
+                  widget.box.title,
+                  style: TextStyle(
+                    fontSize: fontSizeCustom,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
